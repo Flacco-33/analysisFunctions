aa90573c1c0e0379fffd6156393a9e8878631f88
Flacco-33 <lalo_hever@hotmail.com>
2024-10-29 17:25:40 -0600

    Eliminar archivos de análisis y actualizar la interfaz de demostración

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import gradio as gr
-# from dataAnalysis import analizar_data_match 
-from function.textAnalysis import analyze_comment
-# from analysisTeacher import analysis_teacher as py
-from function.analysisTeacher import analysis_teacher
-from function.dataAnalysis import analysis_data_match
+from functions.textAnalysis import analyze_comment
 
-iface = gr.Interface(
+demo = gr.Interface(
     fn=analyze_comment,
     inputs="text",
     outputs="text",
@@ -14,24 +10,5 @@ iface = gr.Interface(
     api_name="/analyze_comment"
 )
 
-iface2 = gr.Interface(
-    fn=analysis_data_match,
-    inputs=None,
-    outputs="text",
-    title="Emotion Match Analysis",
-    description="Analyze the matches between the emotions of students and teachers.",
-    api_name="/analysis_data_match"
-)
-
-iface3=gr.Interface(
-    fn=analysis_teacher,
-    inputs=None,
-    outputs="text",
-    title="Teacher Comments Analysis",
-    description="Analyze the comments of the teachers.",
-    api_name="/analysis_teacher"
-)
-
-demo = gr.TabbedInterface([iface, iface2,iface3], ["Text-to-text", "Emotion Match Analysis","Analysis Teacher"])
 if __name__ == "__main__":
-    demo.launch(server_port=7862)
+    demo.launch(server_port=7863)
